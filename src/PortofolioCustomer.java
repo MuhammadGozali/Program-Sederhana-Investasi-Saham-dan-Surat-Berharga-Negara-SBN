5985fc1f836e3e568e8ef3d3fe7b7f7733aa1c4f
MuhammadGozali <gojalipcs@gmail.com>
2025-04-30 00:18:25 +0800

    PortofolioCustomer class

---
+++ src/PortofolioCustomer.java
@@ -0,0 +1,7 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class PortofolioCustomer {
+    Map<String, Integer> sahamDimiliki = new HashMap<>();
+    Map<String, Double> sbnDimiliki = new HashMap<>();
+}
